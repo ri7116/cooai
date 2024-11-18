f64290ec132ed264c91769854edda6b479bc46e2
Cokekookie <ri7116@naver.com>
2024-11-18 20:04:24 +0900

    test

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from flask import Flask, jsonify, request
 app = Flask(__name__)
 
 # 샘플 데이터
-items = ["1234","123"]
+items = ["1234", "123"]
 
 # GET: 데이터 가져오기
 @app.route('/items', methods=['GET'])
@@ -18,4 +18,5 @@ def add_item():
     return jsonify(data), 201
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    # 포트 번호를 4000으로 변경
+    app.run(host='0.0.0.0', port=4000, debug=True)
